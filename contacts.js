65df924d326dd3ddf22bed59bf7cec5a1808e5d8
Obo Agboghidi <obo@Obos-MacBook-Pro.local>
2017-07-13 10:23:27 -0700

    2nd Push

--- contacts.js
+++ contacts.js
@@ -1,47 +1,5 @@
-let allContacts = []
-
-const addContact = function(firstName, lastName, email) {
-  console.log('addContact:', 'TODO')
-}
-
-const addContacts = function(contactData) {
-  console.log('addContacts:', 'TODO')
-  for(var i = 0; i < contactData.length; i++){
-    allContacts.push(contactData[i].first_name + ' ' + contactData[i].last_name + '  |' + contactData[i].email);
-    //allContacts.push(contactData[i].email)
-  //var full_name = contactData[i].first_name + '|' + contactData[i].last_name + '|' + contactData[i].email;
-  }
-  allContacts.sort();
-  //console.log(allContacts);
-  return allContacts;
-}
-
-const printContacts= function(contacts) {
-  //console.log('printContacts:', 'TODO');
-  console.log("Loading contact data...");
-  console.log("...Finished loading contact data.");
-
-
-  console.log("Full Name           | Email");
-  console.log(allContacts);
-}
-
-
-
-
-////////
-//Test
-/*
-let array = ["Tanny","Tova","Engracia","Conroy"];
-function arraySort(){
-  console.log(array.sort());
-}
-arraySort();
-*/
-
-///////////////////////////////////////////////////////////////////////////
-
-addContacts([{
+let allContacts = [];
+let contactData = ([{
   "first_name": "Tanny",
   "last_name": "Vibert",
   "email": "tvibert0@illinois.edu"
@@ -122,5 +80,58 @@ addContacts([{
   "last_name": "Hixley",
   "email": "whixleyj@homestead.com"
 }])
+
+const addContact = function(firstName, lastName, email) {
+  console.log('addContact:', 'TODO');
+  let contact = {};
+  contact.first_name = firstName;
+  contact.last_name = lastName;
+  contact.email = email;
+  console.log(contact)
+  contactData.push(contact)
+}
+
+
+addContact("John", "Doe", "fakeNews@gmail.com");
+
+const addContacts = function(contactData) {
+  console.log('addContacts:', 'TODO')
+  for(var i = 0; i < contactData.length; i++){
+    allContacts.push(contactData[i].first_name + ' ' + contactData[i].last_name + '  |' + contactData[i].email);
+    //allContacts.push(contactData[i].email)
+  //var full_name = contactData[i].first_name + '|' + contactData[i].last_name + '|' + contactData[i].email;
+  }
+  allContacts.sort();
+  //console.log(allContacts);
+  return allContacts;
+}
+
+const printContacts= function(contacts) {
+  //console.log('printContacts:', 'TODO');
+  console.log("Loading contact data...");
+  process.stdout.write("...Finished loading contact data.");
+
+
+  console.log("Full Name           | Email");
+  console.log(allContacts);
+}
+
+
+
+
+////////
+//Test
+/*
+let array = ["Tanny","Tova","Engracia","Conroy"];
+function arraySort(){
+  console.log(array.sort());
+}
+arraySort();
+*/
+
+///////////////////////////////////////////////////////////////////////////
+
+
+addContacts(contactData);
 
 printContacts()
